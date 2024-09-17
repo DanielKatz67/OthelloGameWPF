3085b8150001d143ae3661dca0ec36f73563d10b
Daniel <danielka1010@gmail.com>
2024-09-17 12:46:12 +0300

    Add GameSettingsForm

--- OthelloGameWPF/OthelloGameWPF/GameSettingsForm.cs
+++ OthelloGameWPF/OthelloGameWPF/GameSettingsForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace OthelloGameWPF
 {
-    public partial class Form1 : Form
+    public partial class GameSettingsForm : Form
     {
-        public Form1()
+        public GameSettingsForm()
         {
             InitializeComponent();
         }

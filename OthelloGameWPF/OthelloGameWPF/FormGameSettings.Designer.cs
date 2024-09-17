05852ac4cd235679207e4f71e1073e50735ee7c5
Daniel <danielka1010@gmail.com>
2024-09-17 18:43:59 +0300

    Refuctor game settings

--- OthelloGameWPF/OthelloGameWPF/FormGameSettings.Designer.cs
+++ OthelloGameWPF/OthelloGameWPF/FormGameSettings.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class FormGameSettings
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,10 +15,6 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.BoardSizeButton = new System.Windows.Forms.Button();
@@ -84,6 +73,5 @@
         private System.Windows.Forms.Button BoardSizeButton;
         private System.Windows.Forms.Button PlayAgainstComputerButton;
         private System.Windows.Forms.Button PlayAgainstFriendButton;
-        
     }
 }
--- OthelloGameWPF/OthelloGameWPF/FormGameSettings.cs
+++ OthelloGameWPF/OthelloGameWPF/FormGameSettings.cs
@@ -15,10 +15,12 @@ namespace OthelloGameWPF
         private void buttonBoardSize_Click(object sender, EventArgs e)
         {
             m_BoardSize += 2;
+
             if (m_BoardSize > 12)
             {
                 m_BoardSize = 6;
             }
+
             BoardSizeButton.Text = $"Board Size: {m_BoardSize}x{m_BoardSize} (click to increase)";
         }
 
